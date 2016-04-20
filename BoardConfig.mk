86983fc8952f7576cb88bcdab748d03d80fdc6f4
Matthew Opperman <mattopp17@gmail.com>
2016-04-20 03:33:35 -0400

    add kernel/rom TC's
    Change-Id: I74cc0cbd2b2cf6540ac4895ccd9d1825282e94a7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,6 +31,13 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/vs985/bluetooth
 TARGET_KERNEL_CONFIG := cyanogenmod_vs985_defconfig
 TARGET_REQUIRES_BUMP := true
 
+# Kernel Toolchain
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-linux-androideabi-4.9/bin
+KERNEL_TOOLCHAIN_PREFIX := arm-linux-androideabi-
+
+# Rom Toolchain
+TARGET_GCC_VERSION_EXP := 4.9
+
 # Optimizations
 CLANG_O3 := true
 STRICT_ALIASING := false
