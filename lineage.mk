3e092ff7e86dd787135138f99435791287c6d59e
Shaun <shaunsibu@gmail.com>
2017-01-23 19:02:13 -0700

    CM-->Lineage
    Change-Id: Id24877abe572dfbf911e45f8cac9098c0b22d5dc

--- lineage.mk
+++ lineage.mk
@@ -3,7 +3,7 @@ $(call inherit-product, device/lge/vs985/full_vs985.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_vs985
+PRODUCT_NAME := lineage_vs985
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
